c50940fa802166e6661df206cdc913c9ab925906
Crafty9813 <152799124+Crafty9813@users.noreply.github.com>
2025-02-16 12:59:34 -0500

    Create BaseGame.java

---
+++ BaseGame.java
@@ -0,0 +1,24 @@
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+
+public abstract class BaseGame extends Game
+{
+    private static BaseGame game;
+  
+    public BaseGame() 
+    {        
+        game = this;
+    }
+    
+    public void create() 
+    {        
+        InputMultiplexer im = new InputMultiplexer();
+        Gdx.input.setInputProcessor( im );
+    }
+    
+    public static void setActiveScreen(BaseScreen s)
+    {
+        game.setScreen(s);
+    }
+}
